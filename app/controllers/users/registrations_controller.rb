fdfc7afaad994f546566d81172681b7596b20d43
Alfred Chang <alfredc_1092@outlook.com>
2017-06-17 06:53:35 +0000

    Added space between before_action and comments

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,5 +1,6 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :select_plan, only: :new
+  
   # Extend default Devise gem behaviour so that 
   # users signing up with the pro account (plan ID 2)
   # save with a special Stripe subscription function;
